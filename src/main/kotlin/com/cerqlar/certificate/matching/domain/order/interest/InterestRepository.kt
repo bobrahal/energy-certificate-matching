62fb98e17a856f4e9c0dd49bcd2217d1158783c6
bobi rahal <bob.rahal@gmail.com>
2021-05-16 01:32:10 +0300

    Change variable name from certificateBundle to interest

--- src/main/kotlin/com/cerqlar/certificate/matching/domain/order/interest/InterestRepository.kt
+++ src/main/kotlin/com/cerqlar/certificate/matching/domain/order/interest/InterestRepository.kt
@@ -4,6 +4,6 @@ import com.cerqlar.certificate.matching.contants.Status
 
 interface InterestRepository {
     fun fetchInterestById(id: InterestId) : Interest?
-    fun save(certificateBundle: Interest) : Interest
+    fun save(interest: Interest) : Interest
     fun update(status: String, id: Long)
 }
