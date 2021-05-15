9548e51c3c6505e8539d0c944724f053212c4ead
bobi rahal <bob.rahal@gmail.com>
2021-05-16 01:01:24 +0300

    Added comments on the ini() function

--- src/main/kotlin/com/cerqlar/certificate/matching/Application.kt
+++ src/main/kotlin/com/cerqlar/certificate/matching/Application.kt
@@ -24,29 +24,33 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 class Application {
     private val log = LoggerFactory.getLogger(Application::class.java)
 
+    /**
+     * Creating some data for Sellers, Buyers and Energy source
+     * This is only for the sake of simplicity and should not be adopted for production apps
+     */
     @Bean
     fun init(
         sellerRepository: SellerCRUDRepository,
         energySourceRepository: EnergySourceCRUDRepository,
         buyerRepository: BuyerCRUDRepository
     ) = CommandLineRunner {
-            // save a couple of energy sources
-            energySourceRepository.save(EnergySourceEntity(id = 1L, code = "SLR", name = "Solar"))
+        // save a couple of energy sources
+        energySourceRepository.save(EnergySourceEntity(id = 1L, code = "SLR", name = "Solar"))
         energySourceRepository.save(EnergySourceEntity(id = 2L, code = "WND", name = "Wind"))
         energySourceRepository.save(EnergySourceEntity(id = 3L, code = "WTR", name = "Water"))
 
         // save a couple of sellers
-            sellerRepository.save(SellerEntity(id = 1L, code = "00001", name = "Seller 1"))
-            sellerRepository.save(SellerEntity(id = 2L, code = "00002", name = "Seller 2"))
-            sellerRepository.save(SellerEntity(id = 3L, code = "00003", name = "Seller 3"))
-            sellerRepository.save(SellerEntity(id = 4L, code = "00004", name = "Seller 4"))
-            sellerRepository.save(SellerEntity(id = 5L, code = "00005", name = "Seller 5"))
+        sellerRepository.save(SellerEntity(id = 1L, code = "00001", name = "Seller 1"))
+        sellerRepository.save(SellerEntity(id = 2L, code = "00002", name = "Seller 2"))
+        sellerRepository.save(SellerEntity(id = 3L, code = "00003", name = "Seller 3"))
+        sellerRepository.save(SellerEntity(id = 4L, code = "00004", name = "Seller 4"))
+        sellerRepository.save(SellerEntity(id = 5L, code = "00005", name = "Seller 5"))
 
-            // save a couple of buyers
-            buyerRepository.save(BuyerEntity(id = 1L, code = "00001", name = "Buyer 1"))
-            buyerRepository.save(BuyerEntity(id = 2L, code = "00002", name = "Buyer 2"))
-            buyerRepository.save(BuyerEntity(id = 3L, code = "00003", name = "Buyer 3"))
-        }
+        // save a couple of buyers
+        buyerRepository.save(BuyerEntity(id = 1L, code = "00001", name = "Buyer 1"))
+        buyerRepository.save(BuyerEntity(id = 2L, code = "00002", name = "Buyer 2"))
+        buyerRepository.save(BuyerEntity(id = 3L, code = "00003", name = "Buyer 3"))
+    }
 }
 
 fun main(args: Array<String>) {
