4f83092af1bb99cbd415981cf664f765a896fed1
bobi rahal <bob.rahal@gmail.com>
2021-05-16 01:18:51 +0300

    Added comments to explain the matching logic implemented

--- src/main/kotlin/com/cerqlar/certificate/matching/service/OrderBookService.kt
+++ src/main/kotlin/com/cerqlar/certificate/matching/service/OrderBookService.kt
@@ -30,14 +30,20 @@ class OrderBookService(
                 message = "certificate energy source ${interest.certificateCode} not found"
             )
 
-        // get the list of certificate bundles available for this energy source ordered by quantity descending
+        // get the list of pending certificate bundles available for this energy source ordered by quantity descending
+        // the list is order by descending quantity, giving priority to the certificate bundles with the largest quantity for a specific energy source
         val certificateBundles: List<CertificateBundle> =
             certificateBundleRepository.findAllByEnergySourceAndStatus(energySource, Status.PENDING)
 
-        var totalSellQuantity: Int = 0
-        var orderBooks: MutableList<OrderBook> = mutableListOf()
-        var matchFound: Boolean = false
+        var totalSellQuantity = 0
+        val orderBooks: MutableList<OrderBook> = mutableListOf()
+        var matchFound = false
 
+        /**
+         * loop through the fetched certificate bundles
+         * break this loop only when the total quantity of the certificate bundles for the same energy source is greater or equal to the quantity in the interest
+         * (meaning that when the interest can be fully matched)
+         */
         for (certificateBundle in certificateBundles) {
 
             totalSellQuantity += certificateBundle.quantity
@@ -47,10 +53,10 @@ class OrderBookService(
                 matchFound = true
                 orderBooks.add(
                     OrderBook(
-                        OrderBookId(0L),
+                        OrderBookId(0L), // id will be auto-generated anyway in db
                         interest,
                         certificateBundle,
-                        certificateBundle.quantity - (totalSellQuantity - interest.quantity), // remaining amount
+                        certificateBundle.quantity - (totalSellQuantity - interest.quantity), // remaining amount to fully close the interest
                         when (totalSellQuantity) {
                             interest.quantity -> MatchType.FULL
                             else -> MatchType.PARTIAL
@@ -59,30 +65,41 @@ class OrderBookService(
                 )
                 break
             } else {
-
+                // if total quantity is still less than interest's quantity, add it in the orderBook list and move on to the next iteration
                 orderBooks.add(
                     OrderBook(
                         OrderBookId(0L),
                         interest,
                         certificateBundle,
-                        certificateBundle.quantity,
+                        certificateBundle.quantity, // order book entry quantity is also the certificate bundle quantity since it's still less than the interest quantity
                         MatchType.FULL
                     )
                 )
             }
         }
 
+        // iterated over the full list and no match was found
+        // interest not matched. Throw OrderNotMatchedException exception
         if (!matchFound) {
             throw OrderNotMatchedException(
                 message = "no combination of certificate bundles found that match interest"
             )
         }
 
+        // Match found,
         handleMatch(interest, orderBooks)
 
+        // return successful event
         return OrderMatchedEvent(orderBooks)
     }
 
+    /**
+     * Close interest
+     * for each entry in the order book
+     *  - log the order book entry in db
+     *  - Close certificate bundle if matchType is FULL,
+     *  otherwise, decrement the quantity by the remaining quantity
+     */
     fun handleMatch(interest: Interest, orderBooks: MutableList<OrderBook>) {
 
         // close interest
